53647e03dda8c9983a88d55237b4001a89651c63
afolabi-ola <afolabiquadri28@gmail.com>
2024-09-20 15:05:09 +0100

    fixed build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
+// import eslint from 'vite-plugin-eslint';
 import Checker from 'vite-plugin-checker'; // Import vite-plugin-checker
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    eslint({ emitWarning: true, emitError: true, failOnError: false }),
+    // eslint({ emitWarning: true, emitError: true, failOnError: false }),
     Checker({ typescript: true }), // Add Checker plugin for TypeScript
   ],
   resolve: {
